1f2e7af91fa76d12060bf5cca34306283d1e40ba
mcartwright <mcartwright@gmail.com>
2018-02-22 12:12:36 -0500

    Updated package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='hearing_screening',
-    version='1.0',
+    version='1.0.1dev1',
     packages=['hearing_screening'],
     include_package_data=True,
     install_requires=[
