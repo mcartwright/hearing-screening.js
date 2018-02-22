e774efe8a1783383e8f98c2538ca4bec78c70d4b
mcartwright <mcartwright@gmail.com>
2018-02-22 12:06:10 -0500

    Set SQLALCHEMY_TRACK_MODIFICATIONS to False to remove deprecation warning

--- hearing_screening/__init__.py
+++ hearing_screening/__init__.py
@@ -7,6 +7,7 @@ import os
 
 app = Flask(__name__, static_url_path='')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:////%s' % os.path.expanduser('~/hearing_screening.db'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 import hearing_screening.views
